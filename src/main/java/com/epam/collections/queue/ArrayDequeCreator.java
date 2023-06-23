0da4543753b44cd62bc4055c07679806d250274f
Сункар Бауыржанулы <sunkar2710@gmail.com>
2023-06-23 12:19:25 +0600

    feat: implement stage1-module4-collections-task4-queue

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,32 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+
+        ArrayDeque<Integer> combinedCards = new ArrayDeque<>();
+
+        int currentPlayer = 1;
+
+        combinedCards.offer(Objects.requireNonNull(firstQueue.poll()));
+        combinedCards.offer(Objects.requireNonNull(firstQueue.poll()));
+        combinedCards.offer(Objects.requireNonNull(secondQueue.poll()));
+        combinedCards.offer(Objects.requireNonNull(secondQueue.poll()));
+
+        while (!firstQueue.isEmpty() || !secondQueue.isEmpty()) {
+            if (currentPlayer == 1) {
+                firstQueue.offer(combinedCards.pollLast());
+                combinedCards.offer(Objects.requireNonNull(firstQueue.poll()));
+                combinedCards.offer(Objects.requireNonNull(firstQueue.poll()));
+            } else {
+                secondQueue.offer(combinedCards.pollLast());
+                combinedCards.offer(Objects.requireNonNull(secondQueue.poll()));
+                combinedCards.offer(Objects.requireNonNull(secondQueue.poll()));
+            }
+            currentPlayer = 3 - currentPlayer;
+        }
+
+        return combinedCards;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,28 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> dishOrder = new ArrayList<>();
+        Queue<Integer> dishes = new LinkedList<>();
+
+        for (int i = 1; i <= numberOfDishes; i++) {
+            dishes.add(i);
+        }
+
+        int count = 1;
+        while (!dishes.isEmpty()) {
+            if (count % everyDishNumberToEat == 0) {
+                int dish = dishes.poll();
+                dishOrder.add(dish);
+            } else {
+                int dish = dishes.poll();
+                dishes.add(dish);
+            }
+            count++;
+        }
+
+        return dishOrder;
     }
 }
